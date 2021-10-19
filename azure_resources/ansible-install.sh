59ee1e109b390c9316194cb658f1a9fd8dc8fa8e
root <root@ansiblectrl.a0ldoacjzu2e3b3yaezsvs1kah.fx.internal.cloudapp.net>
2021-10-19 14:13:06 +0000

    VM creation success

--- azure_resources/ansible-install.sh
+++ azure_resources/ansible-install.sh
@@ -11,3 +11,14 @@ pip3 install ansible[azure]
 /usr/local/bin/ansible-galaxy collection install azure.azcollection
 wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
 pip3 install -r requirements-azure.txt
+
+# Install azure cli
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
+
+yum install -y azure-cli
